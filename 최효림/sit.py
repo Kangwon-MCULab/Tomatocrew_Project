0a872bddb8af259f124b9196e44892699c1e8db7
hyolim112 <hyorimchoi835@gmail.com>
2021-07-07 23:39:40 +0900

    0707

--- 최효림/sit.py
+++ 최효림/sit.py
@@ -5,6 +5,7 @@ class sit():
     def __init__(self):
         self.n = 40
         self.data_s = ([])
+        self.sitnum = 0
 
         self.count = 0
 
@@ -14,11 +15,16 @@ class sit():
         self.data_s= np.append(self.data_s,data[4])
         return
 
+
+
     def think(self):
         print("enter")
         for i in range(0,len(self.data_s)-1):
             if((self.data_s[i]- self.data_s[i+1]) >= 200000):
                 self.count = self.count + 1 
-                print(self.count)
+                if(self.count%2 == 0):
+                    self.sitnum = self.count/2
+                    print("===============================================",self.sitnum ,"번 앉았다 일어났습니다.")
+                
         self.data_s = np.array([])
         return
